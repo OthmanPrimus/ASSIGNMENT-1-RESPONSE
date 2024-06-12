81c8fcdc49a72de4fe05afb09345bffce83e4fd2
OthmanPrimus <othprimus123@gmail.com>
2024-06-12 09:27:44 +0300

    Initial commit

--- app/src/main/java/com/example/assignment/MainActivity.java
+++ app/src/main/java/com/example/assignment/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.assignment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        EditText fullName= findViewById(R.id.signup_firstname);
+        EditText fullName= findViewById(R.id.editTextText2);
+        EditText fullname= findViewById(R.id.editTextTextEmailAddress);
+        EditText fullname= findViewById(R.id.editTextTextPassword);
+        EditText fullname=findViewById(R.id.editTextTextPassword2);
+
+//        Button button = findViewById(R.id.signup_button);
+//        button.setOnClickListener(new View.OnClickListener(){
+//            public void onClick(View v){
+//                //Toast.makeText(content this, text"JENNIFER" int=)
+           }
+       });
+
+
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
     }
 }
